0675752cb9457c78254d3563231c562315cef7af
oe0315 <oeoe0315@gmail.com>
2020-03-26 03:50:56 +0900

    [update]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -40,6 +40,17 @@ class BooksController < ApplicationController
 		end
 	end
 
+	def update
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+      flash[:notice] = 'You have updated book successfully.'
+  	  redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render 'index'
+    end
+	end
+
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update, :index]
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
+  resources :books
   root to: 'tops#home'
   get "home/about" => "tops#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
